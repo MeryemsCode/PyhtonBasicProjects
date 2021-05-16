6b998ca844eae9dbb38c0e53eb2b1d703b34c2b3
MeryemYalvacSoft <meryemyalvac91@gmail.com>
2021-05-17 01:46:40 +0300

    Create turtle_M_Harfi.py

---
+++ 02_Second_Project/turtle_M_Harfi.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.bgcolor("red")
+turtle.color("black")
+turtle.pensize(3)
+turtle.left(90)
+turtle.fd(200)
+turtle.right(135)
+turtle.fd(100)
+turtle.left(90)
+turtle.fd(100)
+turtle.right(135)
+turtle.fd(200)
