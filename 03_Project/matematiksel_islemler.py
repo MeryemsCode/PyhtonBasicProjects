3479fb9a225d38c9c090336cc0123d15e6fc0d93
MeryemYalvacSoft <meryemyalvac91@gmail.com>
2021-05-26 21:56:50 +0300

    Test

---
+++ 03_Project/matematiksel_islemler.py
@@ -0,0 +1,24 @@
+#Ortalama
+AlininBoyu=int(180.5) #int yazdigimiz icin 180 olarak aldi
+AyseninBoyu=int(160)
+FatmaninBoyu=int(172)
+MehmetinBoyu=int(168)
+ortalama=(AlininBoyu+AyseninBoyu+FatmaninBoyu+MehmetinBoyu)/4
+
+print("Ortalama",ortalama)
+
+
+#Dikdörtgenin alani
+KisaKenar=int(50)
+UzunKenar=int(150)
+Alan=(KisaKenar*UzunKenar)
+
+print("Alan",Alan)
+
+
+#Dairenin cevresi ve alani
+Yaricap=2
+Cevre=2*3.14*Yaricap
+Alan=(3.14*Yaricap*Yaricap)
+
+print("Dairenin cevresi",Cevre,"ve alani",Alan,"dir")
