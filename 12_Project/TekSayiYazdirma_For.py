f0fe5ec6e8e2e1072f6ec8d1226e974c100e22a9
MeryemYalvacSoft <meryemyalvac91@gmail.com>
2021-06-10 00:29:39 +0300

    TekSayiYazdirma_For.py

---
+++ 12_Project/TekSayiYazdirma_For.py
@@ -0,0 +1,8 @@
+toplam=0
+
+for x in range(1,100,2):                #1'den 100e kadar(100 dahil değil) 2ser ilerle
+    print("x: ",x)
+    toplam=toplam+x
+    print("sıralı toplam: ",toplam)
+
+print("toplam: ",toplam)
