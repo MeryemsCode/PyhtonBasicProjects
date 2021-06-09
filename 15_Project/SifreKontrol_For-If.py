a8af28b52f178bcbe7a6df5e4f6f9e7ebb0be195
MeryemYalvacSoft <meryemyalvac91@gmail.com>
2021-06-10 01:13:11 +0300

    SifreKontrol_For-If.py

---
+++ 15_Project/SifreKontrol_For-If.py
@@ -0,0 +1,29 @@
+#şifre en az 8 karakter, en az 1 büyük harf, en az 1 küçük harf, en az 1 sayıdan olusmalı
+
+sifre=input("Lütfen şifre giriniz: ")
+buyuk="ABCDEFGHIJKLMNOPRSTUVYZWXQ"   #buyuk harf listesi olusturduk
+kucuk="abcdefghijklmnoprstuvyzwxq"   #küçük harf listesi olusturduk
+digit="0123456789"                  #sayı listesi olusturduk
+
+bhsayi=0                            #kaç adet büyük harf var
+khsayi=0                            #kaç adet küçük harf var
+dsayi=0                             #kaç adet sayı var
+
+for harf in sifre:                  #şifredeki her bir harf için
+    if harf in buyuk:               #eğer büyük harf ise, sayilari bir arttır
+        bhsayi=bhsayi+1
+
+    if harf in kucuk:
+        khsayi=khsayi+1
+
+    if harf in digit:
+        dsayi=dsayi+1
+
+#döngü bitti
+ 
+#eğer büyük harf yoksa ya da küçük harf yoksa ya da sayı yoksa ya da şifre 8 karakterden az ise;
+
+if (bhsayi==0) or (khsayi==0) or (dsayi==0) or (len(sifre)<8): 
+    print("GÜVENSİZ ŞİFRE! TEKRAR DENEYİNİZ")
+else:
+    print("GÜVENLİ ŞİFRE")
